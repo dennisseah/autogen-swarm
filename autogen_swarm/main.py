9a86bad256aa384dcc3c7e585de672b8f9ead4bc
Dennis Seah <dennis.seah@microsoft.com>
2025-03-07 22:43:40 -0800

    refine the prompts

--- autogen_swarm/main.py
+++ autogen_swarm/main.py
@@ -1,7 +1,11 @@
 import asyncio
 
 from autogen_agentchat.agents import AssistantAgent
-from autogen_agentchat.conditions import HandoffTermination, TextMentionTermination
+from autogen_agentchat.conditions import (
+    HandoffTermination,
+    MaxMessageTermination,
+    TextMentionTermination,
+)
 from autogen_agentchat.messages import HandoffMessage
 from autogen_agentchat.teams import Swarm
 from autogen_agentchat.ui import Console
@@ -22,13 +26,14 @@ def get_banking_teller_agent(
     return AssistantAgent(
         "banking_teller_agent",
         model_client=model_client,
-        tools=[get_bank_account_id],
-        handoffs=["saving_account_agent", "investment_account_agent", "user"],
-        system_message="""You are a customer service representative, helps customers
-with financial transactions. You need to get the customer bank account ID first and then
-handoff to the other agents for assistance. The account ID for saving and investment
-account are the same. When customer's request is completed, you can respond with
-TERMINATE.""",
+        handoffs=["saving_account_agent", "user"],
+        system_message="""You are a customer service representative with helps customers
+with getting account balances. Follow these instructions
+
+1  handoff to the saving account agent to fetch saving account balance
+2. handoff to the investment account agent to fetch investment account balance
+
+When customer's request is completed, you can respond with TERMINATE.""",
     )
 
 
@@ -38,13 +43,12 @@ def get_saving_account_agent(
     return AssistantAgent(
         "saving_account_agent",
         model_client=model_client,
-        handoffs=["investment_account_agent", "banking_teller_agent"],
-        tools=[get_saving_account_balance],
-        system_message="""You are a saving account banking agent.
-You have information about the user's saving account balance. You shall provide the
-saving account balance when customer ask for it. You handoff to the investment account
-agent only if customer asks for investment account balance, otherwise handoff to
-banking teller.""",
+        handoffs=["investment_account_agent"],
+        tools=[get_bank_account_id, get_saving_account_balance],
+        system_message="""You are a saving account banking agent. You need the bank
+account ID. You have information about the user's saving account balance. You shall
+provide the saving account balance and then you can handoff to the investment account
+agent.""",
     )
 
 
@@ -55,10 +59,11 @@ def get_investment_account_agent(
         "investment_account_agent",
         model_client=model_client,
         handoffs=["banking_teller_agent"],
-        tools=[get_investment_account_balance],
-        system_message="""You are a investment account banking agent.
-You have information about the user's investment account balance. You shall provide
-the investment account balance and then you can handoff to the banking teller.""",
+        tools=[get_bank_account_id, get_investment_account_balance],
+        system_message="""You are a investment account banking agent. You need the bank
+account ID. You have information about the user's investment account balance. You shall
+provide the investment account balance and then you can handoff to the banking teller.
+""",
     )
 
 
@@ -66,8 +71,10 @@ async def main(task: str):
     openai_service = container[IAzureOpenAIService]
 
     model_client = openai_service.get_model()
-    termination = HandoffTermination(target="user") | TextMentionTermination(
-        "TERMINATE"
+    termination = (
+        HandoffTermination(target="user")
+        | MaxMessageTermination(max_messages=50)
+        | TextMentionTermination("TERMINATE")
     )
     team = Swarm(
         [
